105c3f7defa2f31b542bbc3e7805efea88526795
JP Saliba <jpsaliba@outlook.com>
2023-10-26 19:25:37 -0400

    Properly render user name and pfp when logged in.

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -2,7 +2,10 @@ const loadProfileName = (id) => {
   return Promise.all([getProfile(id)])
     .then(([profile]) => {
       const name = profile[0].display_name;
+      const pfp = profile[0].pfp_url;
       const username = document.querySelector("#user-name");
+      const pfpDisplay = document.querySelector("#user-pfp");
+      pfpDisplay.src = pfp;
       username.textContent = `@${name}`;
     })
     .catch((error) => {
